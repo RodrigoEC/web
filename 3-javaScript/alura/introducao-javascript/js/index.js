5c953ea57fcb3f687e221d73fd94d85bb67a7518
Rodrigo Eloy <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-06-09 21:15:38 -0300

    Aula 01 de js na alura

---
+++ 3-javaScript/alura/introducao-javascript/js/index.js
@@ -0,0 +1,13 @@
+console.log("Hello world!")
+
+// var titulos = document.querySelectorAll(".paciente")
+
+// console.log(titulos.length)
+
+// // for (var titulo = 0; titulo < titulos.length; titulo++) {
+// //     console.log(titulo)
+// // }
+
+var titulo = document.querySelector(".header-titulo")
+
+titulo.textContent = "Aparecida Nutricionista"
