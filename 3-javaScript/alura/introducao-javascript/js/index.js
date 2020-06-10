fb2764f1692f7e880f95d1ddb8e465dc61637411
Rodrigo Eloy <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-06-10 12:07:06 -0300

    Início do cursode introdução a js

--- 3-javaScript/alura/introducao-javascript/js/index.js
+++ 3-javaScript/alura/introducao-javascript/js/index.js
@@ -1,13 +1,54 @@
 console.log("Hello world!")
 
-// var titulos = document.querySelectorAll(".paciente")
+var titulo = document.querySelector(".header-titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-// console.log(titulos.length)
+var PesoEhValido = true;
+var alturaEhvalida = true;
 
-// // for (var titulo = 0; titulo < titulos.length; titulo++) {
-// //     console.log(titulo)
-// // }
+function validaPeso(peso, element) {
+    if (peso.textContent > 300) {
+        console.log("Peso inválido");
 
-var titulo = document.querySelector(".header-titulo")
+        PesoEhValido = false;
+        element.textContent = "Peso inválido";
+        
+        peso.classList.add("dado-invalido")
+    }
+}
 
-titulo.textContent = "Aparecida Nutricionista"
+function validaAltura(altura, element) {
+    if (altura.textContent < 0 || altura.textContent > 3) {
+        console.log("Altura inválida");
+
+        alturaEhvalida = false;
+        element.textContent = "Altura inválida";
+
+        altura.classList.add("dado-invalido")
+    }
+}
+
+function calcIMC() {
+    var pesos = document.querySelectorAll(".info-peso");
+    var alturas = document.querySelectorAll(".info-altura");
+
+    var imcs = document.querySelectorAll(".info-imc");
+
+    for (var i = 0; i < pesos.length; i++) {  
+        let peso = pesos[i].textContent
+        let altura = alturas[i].textContent
+
+        validaAltura(alturas[i], imcs[i]);
+        validaPeso(pesos[i], imcs[i]);
+
+        if (alturaEhvalida && PesoEhValido) {
+            imc = peso / altura**2
+            imcs[i].textContent = imc.toFixed(2);
+        } 
+
+        PesoEhValido = true;
+        alturaEhvalida = true;
+    }
+}
+
+calcIMC();
